f8991f86c0cb265484a3cac2aa7fa10ef41032e2
nathanforester <nathanforester32@gmail.com>
2021-09-23 16:34:57 +0100

    improved logic on days in a month to determine age in months

--- app.py
+++ app.py
@@ -26,23 +26,23 @@ def date():
     currentDay = int(currentDay)
 
     if currentMonth == 4 or 6 or 9 or 11:
-        if birthDays + currentDay > 30:
-            ageInMonths =+ 1 + ageInMonths 
+        if birthDays - currentDay > 0 and birthDays - currentDay < 30:
+            ageInMonths =- 1 + ageInMonths 
         else:
             ageInMonths = ageInMonths
     elif currentMonth == 1 or 3 or 5 or 7 or 8 or 10 or 12:
-        if birthDays + currentDay > 31:
-            ageInMonths =+ 1 + ageInMonths
+        if birthDays - currentDay > 0 and birthDays - currentDay < 31:
+            ageInMonths =- 1 + ageInMonths
         else:
             ageInMonths = ageInMonths
     elif currentMonth == 2:
-        if birthDays + currentDay > 28:
-            ageInMonths =+ 1 + ageInMonths
+        if birthDays - currentDay > 0 and birthDays - currentDay < 28:
+            ageInMonths =- 1 + ageInMonths
         else:
             ageInMonths = ageInMonths
     elif ((birthYear % 4 == 0) and (birthYear % 100 != 0)) or (birthYear % 400 == 0) and currentMonth == 2:
-        if birthDays + currentDay > 29:
-            ageInMonths =+ 1 + ageInMonths
+        if birthDays - currentDay > 0 and birthDays - currentDay < 29:
+            ageInMonths =- 1 + ageInMonths
         else:
             ageInMonths = ageInMonths
 
